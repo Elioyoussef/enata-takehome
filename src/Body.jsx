8368557574a827737f7683ce28483ba7c3276dee
Elio <elio@fahecom.com>
2024-01-27 04:43:19 +0200

    fixing spacing

--- src/Body.jsx
+++ src/Body.jsx
@@ -5,7 +5,7 @@ import JobListingContainer from "./JobListingContainer";
 
 const Body = () => {
   return (
-    <div className="lg:pt-28 lg:px-20 lg:flex-row lg:justify-between flex flex-col justify-center px-4 text-gray-400 h-[100vh]">
+    <div className="lg:pt-28 lg:px-20 lg:flex-row lg:justify-between flex flex-col justify-center px-4 text-gray-400 h-[100vh] pt-6">
       <JoinUsBanner />
       <div className="lg:text-2xl font-thin">
         <p className="text-grey-500">Hey! We're hiring!</p>
